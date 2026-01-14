188fe71ec46183345035157c551ae391083f962d
musiciancoder <andresguitarra1980@gmail.com>
2026-01-14 13:31:22 -0300

    entity, repo y controller funcionando

---
+++ src/main/java/org/acme/controller/CustomerResource.java
@@ -0,0 +1,26 @@
+package org.acme.controller;
+
+import jakarta.annotation.security.PermitAll;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.acme.repository.CustomerRepository;
+
+@Path("/customers")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class CustomerResource {
+    private final CustomerRepository repo;
+    public CustomerResource(CustomerRepository repo) {
+        this.repo = repo;
+    }
+
+    @GET
+    @Path("/{id}")
+    @PermitAll
+    public Response get(@PathParam("id") Long id) {
+        return repo.findByIdOptional(id).map(Response::ok)
+                .orElse(Response.status(404))
+                .build();
+    }
+}
